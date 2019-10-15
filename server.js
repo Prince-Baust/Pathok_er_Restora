b3ea17c4ee09dbddd87e625e9fb3771703e54b4e
Md. Maruf Al Hossain Prince <pmaruf42@gmail.com>
2019-10-16 00:16:40 +0600

    same for who moved my cheese

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-var reviews = [];
+var out_of_the_maze_reviews = [];
+var who_moved_my_cheese_reviews = [];
+var eat_that_frog_reviews = [];
+var the_4_hour_work_week_reviews = [];
+var the_alchemist_reviews = [];
+var the_subtle_art_of_not_giving_a_fck_reviews = [];
 
 app.get('/', function (req, res) {
     res.render("index", {styles: ['styles.css', 'navbar_and_footer.css']});
@@ -36,7 +41,7 @@ app.get('/home', function (req, res) {
 app.get("/out-of-the-maze", function (req, res) {
    res.render("out-of-the-maze", {
        styles: ['single_book.css','navbar_and_footer.css'],
-        reviews: reviews});
+       out_of_the_maze_reviews: out_of_the_maze_reviews});
 });
 
 app.post("/out-of-the-maze", function (req, res) {
@@ -45,8 +50,24 @@ app.post("/out-of-the-maze", function (req, res) {
        email: req.body.user_email,
        review: req.body.user_review
    };
-   reviews.push(review);
+    out_of_the_maze_reviews.push(review);
     res.redirect("/out-of-the-maze");
+});
+
+app.get("/who-moved-my-cheese", function (req, res) {
+    res.render("who-moved-my-cheese", {
+        styles: ['single_book.css','navbar_and_footer.css'],
+        who_moved_my_cheese_reviews: who_moved_my_cheese_reviews});
+});
+
+app.post("/who-moved-my-cheese", function (req, res) {
+    const review = {
+        name: req.body.user_name,
+        email: req.body.user_email,
+        review: req.body.user_review
+    };
+    who_moved_my_cheese_reviews.push(review);
+    res.redirect("/who-moved-my-cheese");
 });
 
 app.listen(3000, function () {
